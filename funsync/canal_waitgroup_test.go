494286f6e74dde4f76f5706ded234b262c516d83
Ramil Gallyamov <gallyamow@gmail.com>
2025-10-15 23:07:48 +0300

    test: bench test moved to test

--- funsync/canal_waitgroup_test.go
+++ funsync/canal_waitgroup_test.go
@@ -217,3 +217,29 @@ func TestCanalWaitGroup_MultipleWaiters(t *testing.T) {
 	}
 	mu.Unlock()
 }
+
+func BenchmarkCanalWaitGroup(b *testing.B) {
+	wg := NewCanalWaitGroup()
+
+	for b.Loop() {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			// имитация работы
+		}()
+	}
+	wg.Wait()
+}
+
+func BenchmarkStandardWaitGroup(b *testing.B) {
+	var wg sync.WaitGroup
+
+	for b.Loop() {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			// имитация работы
+		}()
+	}
+	wg.Wait()
+}
