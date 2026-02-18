8fce17d7a6eaf3a8cf9a4023e182b0de212b1e6a
Ramil Gallyamov <gallyamow@gmail.com>
2026-02-18 18:00:24 +0300

    feat(explore): goexit

--- explore/goexit/goexit_test.go
+++ explore/goexit/goexit_test.go
@@ -5,4 +5,27 @@ import (
 )
 
 func TestGoexit(t *testing.T) {
+	t.Run("calls defers", func(t *testing.T) {
+		done := make(chan struct{})
+		var steps []string
+
+		func() {
+			defer func() {
+				steps = append(steps, "defer1")
+			}()
+			defer func() {
+				steps = append(steps, "defer2")
+			}()
+			close(done)
+		}()
+
+		<-done
+		if len(steps) != 2 {
+			t.Fatalf("expected 2 deferred calls, got %d (%v)", len(steps), steps)
+		}
+		if steps[0] != "defer2" || steps[1] != "defer1" {
+			t.Fatalf("unexpected defer order: expected %v, got %v", []string{"defer2", "defer1"}, steps)
+		}
+
+	})
 }
