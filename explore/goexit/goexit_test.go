b6b79348c778455d18edc149da68e1e639e30bed
Ramil Gallyamov <gallyamow@gmail.com>
2026-02-19 10:25:04 +0300

    feat(explore): goexit

--- explore/goexit/goexit_test.go
+++ explore/goexit/goexit_test.go
@@ -1,6 +1,7 @@
 package goexit
 
 import (
+	"runtime"
 	"testing"
 )
 
@@ -9,7 +10,7 @@ func TestGoexit(t *testing.T) {
 		done := make(chan struct{})
 		var steps []string
 
-		func() {
+		go func() {
 			defer func() {
 				steps = append(steps, "defer1")
 			}()
@@ -17,6 +18,8 @@ func TestGoexit(t *testing.T) {
 				steps = append(steps, "defer2")
 			}()
 			close(done)
+
+			runtime.Goexit()
 		}()
 
 		<-done
@@ -26,6 +29,54 @@ func TestGoexit(t *testing.T) {
 		if steps[0] != "defer2" || steps[1] != "defer1" {
 			t.Fatalf("unexpected defer order: expected %v, got %v", []string{"defer2", "defer1"}, steps)
 		}
+	})
 
+	t.Run("no panic", func(t *testing.T) {
+		done := make(chan struct{})
+		var p any
+
+		go func() {
+			defer func() {
+				if r := recover(); r != nil {
+					p = r
+				}
+			}()
+			close(done)
+
+			runtime.Goexit()
+		}()
+
+		<-done
+		if p != nil {
+			t.Fatalf("expected no panic thrown, got (%v)", p)
+		}
+	})
+
+	t.Run("calls defers", func(t *testing.T) {
+		done := make(chan struct{})
+		var steps []string
+
+		go func() {
+			steps = append(steps, "level1")
+			func() {
+				steps = append(steps, "level2")
+				func() {
+					steps = append(steps, "level3")
+					func() {
+						steps = append(steps, "level4")
+						func() {
+							defer close(done)
+							steps = append(steps, "level5")
+							runtime.Goexit()
+						}()
+					}()
+				}()
+			}()
+		}()
+
+		<-done
+		if len(steps) != 5 {
+			t.Fatalf("expected 5 deferred calls, got %d (%v)", len(steps), steps)
+		}
 	})
 }
